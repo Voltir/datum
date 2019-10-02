c25f59492a322089c655ef4ee37b5c3e85720ac0
Nick Childers <nick@elementanalytics.com>
2019-10-02 11:59:14 -0700

    Allows client apps to use their own upickle API configuration to write schemas

--- ujson/src/main/scala/datum/ujsonlib/schemas/SchemaReadWriter.scala
+++ ujson/src/main/scala/datum/ujsonlib/schemas/SchemaReadWriter.scala
@@ -4,17 +4,19 @@ import datum.patterns.properties._
 import datum.patterns.schemas._
 import datum.ujsonlib.properties.PropertyReadWriter
 import higherkindness.droste.{Algebra, Coalgebra, scheme}
-import upickle.default._
 
 import scala.collection.immutable.SortedMap
 
-trait SchemaReadWriter { self: PropertyReadWriter =>
+class SchemaReadWriter[API <: upickle.Api](implicit val api: API) {
+
+  import api._
+  private implicit val propRW: ReadWriter[Property] = PropertyReadWriter.propertyRW(api)
 
   val algebra: Algebra[SchemaF, ujson.Value] = Algebra {
     case ObjF(fields, properties) =>
       ujson.Obj(
         "fields" -> fields,
-        "properties" -> writeJs(properties)
+        "properties" -> api.writeJs(properties)
       )
 
     case RowF(columns, properties) =>
@@ -28,35 +30,35 @@ trait SchemaReadWriter { self: PropertyReadWriter =>
                 ujson.Obj("schema" -> col.value)
             }
           ),
-        "properties" -> writeJs(properties)
+        "properties" -> api.writeJs(properties)
       )
 
     case ArrayF(element, properties) =>
       ujson.Obj(
         "array" -> element,
-        "properties" -> writeJs(properties)
+        "properties" -> api.writeJs(properties)
       )
 
     case UnionF(alternatives, properties) =>
       ujson.Obj(
         "union" -> alternatives,
-        "properties" -> writeJs(properties)
+        "properties" -> api.writeJs(properties)
       )
 
     case ValueF(tpe, properties) =>
       ujson.Obj(
         "type" -> Type.asString(tpe),
-        "properties" -> writeJs(properties)
+        "properties" -> api.writeJs(properties)
       )
   }
 
   val coalgebra: Coalgebra[SchemaF, ujson.Value] = Coalgebra[SchemaF, ujson.Value] {
     case ujson.Obj(fields) if fields.contains("type") =>
-      val props = read[Map[String, Property]](fields("properties"))
+      val props = api.read[Map[String, Property]](fields("properties"))
       ValueF(Type.fromString(fields("type").str).get, props)
 
     case ujson.Obj(fields) if fields.contains("columns") =>
-      val props = read[Map[String, Property]](fields("properties"))
+      val props = api.read[Map[String, Property]](fields("properties"))
       val elements = fields("columns").arr.view.map { colJs =>
         val header = colJs.obj.get("header").map(_.str)
         Column[ujson.Value](colJs("schema"), header)
@@ -64,31 +66,36 @@ trait SchemaReadWriter { self: PropertyReadWriter =>
       RowF(elements, props)
 
     case ujson.Obj(fields) if fields.contains("array") =>
-      val props = read[Map[String, Property]](fields("properties"))
+      val props = api.read[Map[String, Property]](fields("properties"))
       ArrayF(fields("array"), props)
 
     case ujson.Obj(fields) if fields.contains("fields") =>
-      val props = read[Map[String, Property]](fields("properties"))
+      val props = api.read[Map[String, Property]](fields("properties"))
       ObjF(SortedMap(fields("fields").obj.toSeq: _*), props)
 
     case ujson.Obj(fields) if fields.contains("union") =>
-      val props = read[Map[String, Property]](fields("properties"))
+      val props = api.read[Map[String, Property]](fields("properties"))
       UnionF(SortedMap(fields("union").obj.toSeq: _*), props)
 
     case invalid => throw SchemaReadWriter.InvalidSchemaJson(invalid)
   }
-
-  implicit val scheamReadWrite: ReadWriter[Schema] = upickle.default
-    .readwriter[ujson.Value]
-    .bimap[Schema](schema => {
-      val toJsonFn = scheme.cata(algebra)
-      toJsonFn(schema)
-    }, js => {
-      val fromJsFn = scheme.ana(coalgebra)
-      fromJsFn(js)
-    })
 }
 
 object SchemaReadWriter {
+
+  def apply[API <: upickle.Api](implicit api: API) = {
+    val schemaReadWriter = new SchemaReadWriter[API]
+    val schemaRW: api.ReadWriter[Schema] = api
+      .readwriter[ujson.Value](api.ReadWriter.join(api.JsValueR, api.JsValueW))
+      .bimap[Schema](schema => {
+        val toJsonFn = scheme.cata(schemaReadWriter.algebra)
+        toJsonFn(schema)
+      }, js => {
+        val fromJsFn = scheme.ana(schemaReadWriter.coalgebra)
+        fromJsFn(js)
+      })
+    schemaRW
+  }
+
   case class InvalidSchemaJson(invalid: ujson.Value) extends Exception(s"Could not convert json to schema: $invalid")
 }
